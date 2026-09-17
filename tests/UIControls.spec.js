29033cc7dba4ebd52971d4733f28e1ac6201c347
Bhavin Bhatia <bhavinbhatia4@gmail.com>
2026-09-17 13:27:49 -0400

    Handling child windows and tabs

--- tests/UIControls.spec.js
+++ tests/UIControls.spec.js
@@ -1,31 +1,29 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
+test("UIControl test", async ({ page }) => {
+  const radioBtn = page.locator(".radiotextsty");
+  const dropdown = page.locator("select.form-control");
+  const terms = page.locator("input#terms");
+  const documentsRequests = page.locator('[href*="documents-request"]');
 
-test.only('UIControl test', async({page}) =>{
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.locator("#username").fill("rahulshettyacademy");
+  await page.locator("#password").fill("Learning@830$3mK2");
 
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    await page.locator('#username').fill('rahulshettyacademy');
-    await page.locator('#password').fill('Learning@830$3mK2');
+  await radioBtn.nth(1).click();
+  await page.locator("#okayBtn").click();
+  console.log(await radioBtn.nth(1).isChecked());
 
-    const radioBtn = page.locator('.radiotextsty')
-    await radioBtn.nth(1).click();
-    await page.locator('#okayBtn').click();
-    console.log(await radioBtn.nth(1).isChecked());
+  await dropdown.selectOption("consult");
+  await expect(radioBtn.last()).toBeChecked();
 
+  await terms.click();
+  await expect(terms).toBeChecked();
 
-    const dropdown = page.locator('select.form-control');
-    await dropdown.selectOption('consult');
+  await terms.uncheck();
+  expect(await terms.isChecked()).toBeFalsy();
 
-    await expect(radioBtn.last()).toBeChecked();
+  await expect(documentsRequests).toHaveAttribute("class", "blinkingText");
 
-    const terms = page.locator('input#terms')
-    await terms.click();
-    expect(await terms).toBeChecked();
-    
-
-    await terms.uncheck();
-    expect (await terms.isChecked()).toBeFalsy();
-
-    await page.locator('#signInBtn').click();
-
+  //await page.locator('#signInBtn').click();
 });
